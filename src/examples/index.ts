96d1672babf158e066fe62bf44af4a44af6303c5
Mark Odayan <markodayansa@gmail.com>
2022-07-29 16:03:43 +0200

    clear git cache for update

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -2,12 +2,4 @@ import { Provider } from '@src/index';
 
 const { provider } = new Provider('infura', process.env.INFURA_PROJECT_ID as string);
 
-// provider.seedLatestBlock(true);
-// provider.seedBlockByNumber(15122166, false);
 provider.getLatestBlock().then(console.log);
-// provider.seedBlockByNumber(400000);
-
-// provider.getBlockByNumber(12_964_760).then((block) => {
-//   const blockNumber = parseInt(block.number, 16);
-//   exportToJSONFile(block, blockNumber.toString(), '/src/seeder/blocks/legacy');
-// });
